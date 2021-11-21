e08a62cf37f0a5487d8e1227ac5f43402c1ab622
Jean Carlos Ayala <ayalajeancarlos.91@gmail.com>
2021-11-21 22:33:22 +0000

    chore: check ethereum metamask object

--- components/WalletConnect.vue
+++ components/WalletConnect.vue
@@ -38,3 +38,36 @@
     </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      currentAccount: null
+    }
+  },
+  async mounted () {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        this.currentAccount(account);
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+</script>
