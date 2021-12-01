a1abc59115456cd2d4dcc1da0f7d4b7f6969e9c4
Jean Carlos Ayala <ayalajeancarlos.91@gmail.com>
2021-12-01 20:13:10 +0000

    chore: LFG

--- components/InfoSection.vue
+++ components/InfoSection.vue
@@ -4,14 +4,29 @@
       <h2
         class="text-4xl font-serif text-gray-900 tracking-tight leading-10 font-bold sm:text-5xl sm:leading-none md:text-6xl xl:pr-2"
       >
-        ¿Por qué hacemos esto?
+        {{ title }}
       </h2>
       <p
         class="mt-3 text-base text-gray-700 sm:mt-5 sm:text-lg sm:max-w-4xl md:mt-5 md:text-xl lg:mx-0 biactro-gradient"
+        v-html="description"
       >
-        En <span class="font-bold">Biactro Technologies</span> hemos decidido abrirnos a nuevos conceptos de financiación colectiva mediante el fundraising, por este motivo, en lugar de contar con grandes inversores queremos darle la oportunidad a cualquier persona afín a la filosofía del cuidado y la protección del medio ambiente, impulsando una tecnología única a nivel mundial.
-        Con esta idea evitamos dejar el futuro de nuestro planeta a las grandes empresas y lobbys.
       </p>
     </div>
   </div>
 </template>
+<script>
+export default {
+  name: "InfoSection",
+  props: {
+    title: {
+      type: String,
+      default: "¿Por qué hacemos esto?",
+    },
+    description: {
+      type: String,
+      default: `En <span class="font-bold">Biactro Technology</span> hemos decidido abrirnos a nuevos conceptos de financiación colectiva mediante el fundraising, por este motivo, en lugar de contar con grandes inversores queremos darle la oportunidad a cualquier persona afín a la filosofía del cuidado y la protección del medio ambiente, impulsando una tecnología única a nivel mundial.
+        Con esta idea evitamos dejar el futuro de nuestro planeta a las grandes empresas y lobbys.`,
+    },
+  }
+}
+</script>
